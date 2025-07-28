8dd1ea284bbf67167937503e95778e6979e8e600
Jose Luis <jose.perezc17@tectijuana.edu.mx>
2025-07-27 19:59:12 -0700

    new commit

--- src/config/envs.ts
+++ src/config/envs.ts
@@ -9,7 +9,7 @@ export const envs = {
     MONGO_DB_NAME: env.get('MONGO_DB_NAME').required().asString(),
 
     MAILER_SERVICE: env.get('MAILER_SERVICE').required().asString(),
-    MAILER_EMAIL: env.get('MAILER_EMAIL').required().asEmailString(),
-    MAILER_SECRET_KEY: env.get('MAILER_SECRET_KEY').required().asString(),
+    MAILER_EMAIL: env.get('MAILER_EMAIL').asEmailString(),
+    MAILER_SECRET_KEY: env.get('MAILER_SECRET_KEY').asString(),
     
 };
